e3afcf20803817cde5c2c5ff1c246c949a359722
TatiZofka <tatianaziakova@gmail.com>
2022-03-20 16:43:16 +0100

    cleaner code + unit conversion

--- src/script.js
+++ src/script.js
@@ -33,102 +33,114 @@ navigator.geolocation.getCurrentPosition(myPlace);
 function myPlace(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
-  let myCity = `lat=${lat}&lon=${lon}`;
+  let city = `lat=${lat}&lon=${lon}`;
 
-  let apiKey = "14af575613645726e379f956e6774a6e";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?${myCity}&appid=${apiKey}&units=metric`;
+  apiUrl = `https://api.openweathermap.org/data/2.5/weather?${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(changeData);
 
-  axios.get(apiUrl).then(changeTemp);
+  apiUrl = `http://api.openweathermap.org/geo/1.0/reverse?${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(changeCountry);
 }
 
 // search for another city
 
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", changeCity);
+searchForm.addEventListener("submit", newSearch);
 
-function changeCity(event) {
+function newSearch(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#search-input");
-  let city = cityInput.value;
+  city = cityInput.value;
 
-  let apiKey = "14af575613645726e379f956e6774a6e";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(changeData);
 
-  axios.get(apiUrl).then(changeTemp);
-}
-
-function changeTemp(response) {
-  console.log(response);
-  let newCity = response.data.name;
-  let originalCity = document.querySelector("h1");
-  originalCity.innerHTML = newCity;
-
-  let newTemp = Math.round(response.data.main.temp);
-  let originalTemp = document.querySelector("#temp-value");
-  originalTemp.innerHTML = newTemp;
-
-  let newTempMin = Math.round(response.data.main.temp_min);
-  let originalTempMin = document.querySelector("#temp-min");
-  originalTempMin.innerHTML = newTempMin;
-
-  let newTempMax = Math.round(response.data.main.temp_max);
-  let originalTempMax = document.querySelector("#temp-max");
-  originalTempMax.innerHTML = newTempMax;
-
-  let newIcon = response.data.weather[0].icon;
-  let originalIcon = document.querySelector("#icon");
-  originalIcon.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${newIcon}@2x.png`
-  );
-
-  let newSunrise = response.data.sys.sunrise;
-  var date1 = new Date(newSunrise * 1000);
-  var hours1 = date1.getHours();
-  var minutes1 = date1.getMinutes();
-  if (hours1 < 10) {
-    hours1 = `0${hours1}`;
-  }
-  if (minutes1 < 10) {
-    minutes1 = `0${minutes1}`;
-  }
-  newSunrise = `${hours1} : ${minutes1}`;
-  let originalSunrise = document.querySelector("#sunrise");
-  originalSunrise.innerHTML = newSunrise;
-
-  let newSunset = response.data.sys.sunset;
-  var date2 = new Date(newSunset * 1000);
-  var hours2 = date2.getHours();
-  var minutes2 = date2.getMinutes();
-  if (hours2 < 10) {
-    hours2 = `0${hours2}`;
-  }
-  if (minutes2 < 10) {
-    minutes2 = `0${minutes2}`;
-  }
-  newSunset = `${hours2} : ${minutes2}`;
-  let originalSunset = document.querySelector("#sunset");
-  originalSunset.innerHTML = newSunset;
-
-  let newDescription = response.data.weather[0].description;
-  let originalDescription = document.querySelector("#description");
-  originalDescription.innerHTML = newDescription;
-
-  let newHumidity = response.data.main.humidity;
-  let originalHumidity = document.querySelector("#humidity");
-  originalHumidity.innerHTML = newHumidity;
-
-  let newWind = Math.round(response.data.wind.speed);
-  let originalWind = document.querySelector("#wind");
-  originalWind.innerHTML = newWind;
-
-  let apiKey = "14af575613645726e379f956e6774a6e";
-  let apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${newCity}&appid=${apiKey}`;
-
+  apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(changeCountry);
 }
 
-// change country & continent
+function changeData(response) {
+  console.log(response);
+
+  function changeCity(response) {
+    let newCity = response.data.name;
+    let originalCity = document.querySelector("h1");
+    originalCity.innerHTML = newCity;
+  }
+
+  function changeTemp(response) {
+    let newTemp = Math.round(response.data.main.temp);
+    let originalTemp = document.querySelector("#temp-value");
+    originalTemp.innerHTML = newTemp;
+
+    let newTempMin = Math.round(response.data.main.temp_min);
+    let originalTempMin = document.querySelector("#temp-min");
+    originalTempMin.innerHTML = newTempMin;
+
+    let newTempMax = Math.round(response.data.main.temp_max);
+    let originalTempMax = document.querySelector("#temp-max");
+    originalTempMax.innerHTML = newTempMax;
+  }
+
+  function changeEmoji(response) {
+    let newIcon = response.data.weather[0].icon;
+    let originalIcon = document.querySelector("#icon");
+    originalIcon.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${newIcon}@2x.png`
+    );
+  }
+
+  function changeSun(response) {
+    let newSunrise = response.data.sys.sunrise;
+    var date1 = new Date(newSunrise * 1000);
+    var hours1 = date1.getHours();
+    var minutes1 = date1.getMinutes();
+    if (hours1 < 10) {
+      hours1 = `0${hours1}`;
+    }
+    if (minutes1 < 10) {
+      minutes1 = `0${minutes1}`;
+    }
+    newSunrise = `${hours1} : ${minutes1}`;
+    let originalSunrise = document.querySelector("#sunrise");
+    originalSunrise.innerHTML = newSunrise;
+
+    let newSunset = response.data.sys.sunset;
+    var date2 = new Date(newSunset * 1000);
+    var hours2 = date2.getHours();
+    var minutes2 = date2.getMinutes();
+    if (hours2 < 10) {
+      hours2 = `0${hours2}`;
+    }
+    if (minutes2 < 10) {
+      minutes2 = `0${minutes2}`;
+    }
+    newSunset = `${hours2} : ${minutes2}`;
+    let originalSunset = document.querySelector("#sunset");
+    originalSunset.innerHTML = newSunset;
+  }
+
+  function changeConditions(response) {
+    let newDescription = response.data.weather[0].description;
+    let originalDescription = document.querySelector("#description");
+    originalDescription.innerHTML = newDescription;
+
+    let newHumidity = response.data.main.humidity;
+    let originalHumidity = document.querySelector("#humidity");
+    originalHumidity.innerHTML = newHumidity;
+
+    let newWind = Math.round(response.data.wind.speed);
+    let originalWind = document.querySelector("#wind");
+    originalWind.innerHTML = newWind;
+  }
+
+  changeCity(response);
+  changeTemp(response);
+  changeEmoji(response);
+  changeSun(response);
+  changeConditions(response);
+}
 
 function changeCountry(response) {
   let newCountry = response.data[0].country;
@@ -136,28 +148,38 @@ function changeCountry(response) {
   originalCountry.innerHTML = newCountry;
 }
 
-// change metrics
+// change temperature units
 
-function toFarenheit(event) {
+function changeUnits(event) {
   event.preventDefault();
-  let value = document.querySelector("#temp-value").innerText;
-  let newValue = value * 1.8 + 32;
-  newValue = Math.round(newValue);
-  let oldValue = document.querySelector("#temp-value");
-  oldValue.innerHTML = newValue;
+  let newUnits = document.querySelector("#different-units");
+  let originalUnits = document.querySelector("#current-units");
+  let changedUnits = originalUnits.textContent;
+  originalUnits.innerHTML = newUnits.textContent;
+  newUnits.innerHTML = changedUnits;
+
+  //if ((originalUnits.textContent = "F")) {
+  //toFarenheit(event);
+  // }
 }
 
-function toCelsius(event) {
-  event.preventDefault();
-  let value = document.querySelector("#temp-value").innerText;
-  let newValue = (value - 32) * 0.5555556;
-  newValue = Math.round(newValue);
-  let oldValue = document.querySelector("#temp-value");
-  oldValue.innerHTML = newValue;
-}
+//function toFarenheit(event) {
+//event.preventDefault();
+//let value = document.querySelector("#temp-value").innerText;
+//let newValue = Math.round(value * (9 / 5) + 32);
+//let oldValue = document.querySelector("#temp-value");
+//oldValue.innerHTML = newValue;
+//}
 
-let celsius = document.querySelector(".celsius");
-let farenheit = document.querySelector(".farenheit");
+let newUnits = document.querySelector("#different-units");
+newUnits.addEventListener("click", changeUnits);
 
-farenheit.addEventListener("click", toFarenheit);
-celsius.addEventListener("click", toCelsius);
+// global variables
+
+let city = "Rio De Janeiro";
+let units = "metric";
+
+let apiKey = "14af575613645726e379f956e6774a6e";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(changeData);
